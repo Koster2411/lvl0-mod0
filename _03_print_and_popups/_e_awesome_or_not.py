31e757dd77e17d220c52ad617f6082e2e6f37b57
Koster2411 <111485774+Koster2411@users.noreply.github.com>
2023-02-09 02:38:48 +0000

    Update _e_awesome_or_not.py

--- _03_print_and_popups/_e_awesome_or_not.py
+++ _03_print_and_popups/_e_awesome_or_not.py
@@ -1,21 +1,22 @@
-from tkinter import messagebox, simpledialog, Tk
+rom tkinter import messagebox, simpledialog, Tk
 import random
-
+import random
 # Create an if-main code block, *hint, type main then ctrl+space to auto-complete
-    
+if __name__ == '__main__':
     # Make a new window variable, window = Tk()
-    
+    window = Tk()
     # Hide the window using the window's .withdraw() method
-
+    window.withdraw()
     # 1. Make a variable equal to a positive number less than 4 using random.randInt(0, 3)
-    
+    number = random.randInt(0, 3)
     # 2. Print your variable to the console
-    
+    print(number)
     # 3. Get the user to enter something that they think is awesome
-    
+    question = simpledialog.askstring(title='number', prompt='enter somthing awsome')
     # 4. If your variable is  0
         # -- tell the user whatever they entered is awesome!
-        
+    if number == 0:
+        messagebox.showinfo(title='awsome', message='your answer is awsome')
     # 5. If your variable is  1
         # -- tell the user whatever they entered is ok.
     
@@ -26,3 +27,4 @@ import random
         # -- invent your own message to give to the user (be nice).
         
     # Run the window's .mainloop() method
+
